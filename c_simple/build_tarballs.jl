5dc08812ecaa3b65334c99997e242e871cab7d14
Curtis Vogt <curtis.vogt@gmail.com>
2021-02-11 15:55:56 -0600

    [c_simple] Build on experimental platforms

--- c_simple/build_tarballs.jl
+++ c_simple/build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder
 
 name = "c_simple"
-version = v"1.2.3"
+version = v"1.2.4"
 
 # Collection of sources required to build libffi
 sources = [
@@ -16,7 +16,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -29,5 +29,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
 
